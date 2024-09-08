0a8f20f05a41a4887f3dfd643b2902b24cd6336a
flavienbwk <flavien@berwick.fr>
2024-09-07 23:54:45 -0400

    Fixing repochat action

--- action/index.js
+++ action/index.js
@@ -16,7 +16,7 @@ async function sendFileToApi(filePath, apiUrl) {
   const payload = { 'content': content, 'metadata': metadata };
 
   try {
-    console.log(`Sending: ${payload}`);
+    console.log(`Sending: ${JSON.stringify(payload)}`);
     const response = await axios.post(apiUrl, payload, { headers: { 'Content-Type': 'application/json' } });
     return response;
   } catch (error) {
